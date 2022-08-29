8699aa009715a2096106e68fa31a6ce9e9229191
Plasmatic <moses@mosesxu.net>
2022-08-29 02:30:12 -0400

    fhc template: use .txt for I/O so fhc site validates formatting

--- judge/fhc.cpp
+++ judge/fhc.cpp
@@ -1,8 +1,8 @@
 void init_file_io() {
 #ifndef LOCAL
     const string PROBLEM_ID = "prob";
-    freopen((PROBLEM_ID + ".in").c_str(), "r", stdin);
-    freopen((PROBLEM_ID + ".out").c_str(), "w", stdout);
+    freopen((PROBLEM_ID + "_in.txt").c_str(), "r", stdin);
+    freopen((PROBLEM_ID + "_out.txt").c_str(), "w", stdout);
 #endif
 }
 
